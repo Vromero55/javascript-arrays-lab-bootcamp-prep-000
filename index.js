ee17b9a86d6b81ffa6fa63973621793fbf9127f0
Victor Romero <romeroa.victor@yahoo.com>
2020-05-25 19:28:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 function destructivelyAppendKitten(a, b){
+  a=kittens;
+  b="Ralph"
   a.push(b);
   return a;
 }
-destructivelyAppendKitten(kittens, "Ralp");
+//destructivelyAppendKitten(kittens, "Ralp");
 // Add your functions and code here
