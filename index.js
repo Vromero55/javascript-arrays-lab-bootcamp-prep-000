2df8723408fe8b4127d0381e15b3fb2bccc015a6
Victor Romero <romeroa.victor@yahoo.com>
2020-05-25 18:52:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ function destructivelyAppendKitten(a,b){
   a.push(b);
   return a;
 }
+destructivelyAppendKitten(kittens, "Ralph");
 var kittens = ["Milo", "Otis", "Garfield"];
 // Add your functions and code here
